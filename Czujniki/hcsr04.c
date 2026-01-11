7e3da9b3d36fa4b7fbddfe7c8b5a08286b9fb2af
Kuba <kobylarzjakub@gmail.com>
2026-01-11 16:42:40 +0100

    Dzialajacy czujnik z odbiorem w terminalu

---
+++ Czujniki/hcsr04.c
@@ -0,0 +1,10 @@
+/*
+ * hcsr04.c
+ *
+ *  Created on: Jan 11, 2026
+ *      Author: kobyl
+ */
+
+
+
+
---
+++ Czujniki/hcsr04.h
@@ -0,0 +1,16 @@
+/*
+ * hcsr04.h
+ *
+ *  Created on: Jan 11, 2026
+ *      Author: kobyl
+ */
+
+#ifndef HCSR04_H_
+#define HCSR04_H_
+
+#include "stm32f7xx_hal.h"
+
+
+
+
+#endif /* HCSR04_H_ */
